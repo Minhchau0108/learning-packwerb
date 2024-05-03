027df5368b3298e631f7dec8d7819c6d425025eb
Minhchau0108 <chaua@coderpush.com>
2024-05-03 08:46:25 +0700

    feat: add blog package

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   scope :admin do
     resources :users
   end
+  scope 'blogs' do
+    resources :posts
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
